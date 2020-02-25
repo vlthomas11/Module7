0949fd411782218bf5edbdbbe20bf200cc7e183b
VLThomas11 <vthomas2@DMACC.edu>
2020-02-25 14:14:37 -0600

    update

--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -1,6 +1,23 @@
 def make_list():
-    pass
+    alist = []
+    counter = 0
+    while counter < 3:
+        inp = int(get_input())
+        alist.append(inp)
+        counter = counter + 1
 
 
 def get_input():
     pass
+    # count = 0
+    # a = 'i'
+    # b = []
+    # while count < 3:
+    #     a = input("enter a number")
+    #     count = count + 1
+    #     b.append(a)
+    # print(b)
+
+
+if __name__ == '__main__':
+    get_input()
