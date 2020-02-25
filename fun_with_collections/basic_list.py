265ce8d6778271b1c9a0311f25372a7a0cbf3b1c
VLThomas11 <vthomas2@DMACC.edu>
2020-02-25 14:44:11 -0600

    update

--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -1,23 +1,19 @@
 def make_list():
+    """Takes inputs from user and makes them into a list"""
     alist = []
     counter = 0
     while counter < 3:
         inp = int(get_input())
         alist.append(inp)
         counter = counter + 1
+    return alist
+
 
 
 def get_input():
-    pass
-    # count = 0
-    # a = 'i'
-    # b = []
-    # while count < 3:
-    #     a = input("enter a number")
-    #     count = count + 1
-    #     b.append(a)
-    # print(b)
-
+    """Gets input from user"""
+    a = input("enter a number ")
+    return a
 
 if __name__ == '__main__':
-    get_input()
+    make_list()
--- test_collections/test_basic_list.py
+++ test_collections/test_basic_list.py
@@ -1,9 +1,11 @@
 import unittest
 from unittest.mock import patch
-from fun_with_collections import basic_list as topic1
+import fun_with_collections.basic_list as topic1
 
 
 class TestList(unittest.TestCase):
-    @patch('fun_with_collections.topic1.get_input', return_vaule='5')
+    @patch('fun_with_collections.basic_list.get_input', return_value='5')
     def test_make_list(self, input):
         self.assertEqual(topic1.make_list(), [5, 5, 5])
+
+
